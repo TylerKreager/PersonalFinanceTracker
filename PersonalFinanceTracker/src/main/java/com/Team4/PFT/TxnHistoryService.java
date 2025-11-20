97b26d7740fc8ac6ae61ee3020535f54e1698bcf
Tyler Kreager <kreagertyler@gmail.com>
2025-11-19 22:49:44 -0500

    Method for GETall for userId

--- PersonalFinanceTracker/src/main/java/com/Team4/PFT/TxnHistoryService.java
+++ PersonalFinanceTracker/src/main/java/com/Team4/PFT/TxnHistoryService.java
@@ -4,12 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.Team4.PFT.DTOs.TxnHistoryDTO;
 
 @Service
 public class TxnHistoryService {
@@ -52,5 +56,16 @@ public class TxnHistoryService {
 				
 			}
 		}
+	}
+	
+	
+	//Getting all txn data for user and returning it in list.
+	public List<TxnHistoryDTO> getAllHistory(Long userId) throws IOException {
+		List<TxnHistory> txns = txnHistoryRepository.findByUser_userId(userId);
+				
+		return txns.stream()
+				.map(TxnHistoryDTO::new)
+				.collect(Collectors.toList());
+		
 	}
 }
