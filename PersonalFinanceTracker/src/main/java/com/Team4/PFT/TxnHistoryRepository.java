9d209a8deb72a459865ec5d82b56487b3ef8d115
Tyler Kreager <kreagertyler@gmail.com>
2025-11-19 22:50:32 -0500

    Query added for finding all txn's by userId

--- PersonalFinanceTracker/src/main/java/com/Team4/PFT/TxnHistoryRepository.java
+++ PersonalFinanceTracker/src/main/java/com/Team4/PFT/TxnHistoryRepository.java
@@ -1,5 +1,6 @@
 package com.Team4.PFT;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TxnHistoryRepository extends JpaRepository<TxnHistory, UUID>{
-
+	
+	List<TxnHistory> findByUser_userId(Long userId);
 	
 }
