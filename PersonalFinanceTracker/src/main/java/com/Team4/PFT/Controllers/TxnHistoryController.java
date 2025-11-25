9cfc5dcaa1a9b980b438a36dd7c00fe30811b283
Tyler Kreager <kreagertyler@gmail.com>
2025-11-24 23:43:17 -0500

    Added Get between dates and get months total endpoints.

--- PersonalFinanceTracker/src/main/java/com/Team4/PFT/Controllers/TxnHistoryController.java
+++ PersonalFinanceTracker/src/main/java/com/Team4/PFT/Controllers/TxnHistoryController.java
@@ -1,5 +1,6 @@
 package com.Team4.PFT.Controllers;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.Team4.PFT.Services.TxnHistoryService;
 import com.Team4.PFT.DTOs.TxnHistoryDTO;
+import com.Team4.PFT.Entities.TxnHistory;
 
 @RestController
 @RequestMapping("/api/txnHistory")
@@ -39,16 +41,35 @@ public class TxnHistoryController {
 		}
 	}
 	
-	//get all txn data for user.
-	@GetMapping("/view/{userId}")
-	public ResponseEntity<?> viewHistory(@PathVariable Long userId) {
-		
-		try {
-			List<TxnHistoryDTO> history = txnHistoryService.getAllHistory(userId);
-			return ResponseEntity.ok(history);
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body("Fetching history has failed!: " + e.getMessage());
+	@GetMapping("betweenDates")
+	public ResponseEntity<?> getTxnBetweenDates(@RequestParam("startDate") LocalDate startDate, 
+			@RequestParam("endDate") LocalDate endDate,
+			@RequestParam(value = "userId") Long userId){
+		List<TxnHistory> txns;
+		if (userId != null) {
+			txns = txnHistoryService.searchBetweenDates(startDate, endDate, userId);
 		}
+		else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body("User could not be found!");
+		}
+		
+		return ResponseEntity.ok(txns);
 	}
+	
+	@GetMapping("monthsTotal")
+	public ResponseEntity<?> getMonthsTxns(@RequestParam("userId") Long userId) {
+		Double total = txnHistoryService.monthsTotal(userId);
+		return ResponseEntity.ok(total);
+	}
+	//get all txn data for user.
+	/*
+	 * @GetMapping("/view/{userId}") public ResponseEntity<?>
+	 * viewHistory(@PathVariable Long userId) {
+	 * 
+	 * try { List<TxnHistoryDTO> history = txnHistoryService.getAllHistory(userId);
+	 * return ResponseEntity.ok(history); } catch (Exception e) { return
+	 * ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+	 * .body("Fetching history has failed!: " + e.getMessage()); } }
+	 */
 }
